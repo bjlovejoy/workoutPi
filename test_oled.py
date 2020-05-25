5078216b569ada3b7f8c1eb65df0900ffe49b946
Brendon <bjlovejoy0924@gmail.com>
2020-05-25 14:52:51 -0400

    oled test

--- test_oled.py
+++ test_oled.py
@@ -5,40 +5,39 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 
-self.oled = Adafruit_SSD1306.SSD1306_128_64(rst=None)
+oled = Adafruit_SSD1306.SSD1306_128_64(rst=None)
 
-self.oled.begin()    #Initialize library
-self.oled.clear()    #Clear display
-self.oled.display()  #Update display
+oled.begin()    #Initialize library
+oled.clear()    #Clear display
+oled.display()  #Update display
 
 #Create blank image for drawing (mode '1' for 1-bit color)
-self.width = self.oled.width
-self.height = self.oled.height
-self.image = Image.new('1', (self.width, self.height))
+width = oled.width
+height = oled.height
+image = Image.new('1', (width, height))
 
-self.draw = ImageDraw.Draw(self.image)                           #Get drawing object to draw on image
-self.draw.rectangle((0,0,self.width,self.height), outline=0, fill=0)  #Draw a black filled box to clear the image
+draw = ImageDraw.Draw(image)                           #Get drawing object to draw on image
+draw.rectangle((0,0,width,height), outline=0, fill=0)  #Draw a black filled box to clear the image
 
 #Define some constants to allow easy resizing of shapes
 padding = -2
-self.top = padding
-self.bottom = self.height - padding
-self.x = 0  #Move left to right keeping track of the current x position for drawing shapes
+top = padding
+bottom = height - padding
+x = 0  #Move left to right keeping track of the current x position for drawing shapes
 
 #self.font = ImageFont.load_default()
-self.font_small = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 10)
-self.font_norm = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 15)
-self.font_large = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 30)
-self.font_huge = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 40)
+font_small = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 10)
+font_norm = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 15)
+font_large = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 30)
+font_huge = ImageFont.truetype("/home/pi/workoutPi/arial.ttf", 40)
 
 
 #----------------------------------------------------MY TEST AREA----------------------------------------------------
-self.clear_image()
-
 text = "My name is Brendon and I am a big boy that really needs a happy break"
 
 text_list = text.split()
 
-self.draw.multiline_text((self.x, self.top + 20), text, font=self.font_norm, fill=255)
-self.disp()
+draw.multiline_text((x, top + 20), text, font=font_norm, fill=255)
+oled.image(image)
+oled.display()
 
